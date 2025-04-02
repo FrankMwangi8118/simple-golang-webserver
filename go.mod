cc2f83debb0d1ce1a171c76e6ae26d761c7cdf0a
CODIFY <frankmwangi8118@gmail.com>
2025-04-02 11:01:49 +0300

    feat: stripe payment intent

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-app
 
 go 1.22.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/stripe/stripe-go v70.15.0+incompatible
+	github.com/stripe/stripe-go/v81 v81.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,66 +1,92 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/v81/paymentintent"
 )
 
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "hello")
-// }
-// func handler2(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "from handler 2")
-// }
-
-// func password(passs string) error {
-// 	var pass string = "mwas"
-// 	if passs == pass {
-// 		return nil
-// 	} else {
-// 		return errors.New("invalid password")
-// 	}
-// }
-
-// func main() {
-// 	// var err error = password("d")
-// 	// if err != nil {
-// 	// 	fmt.Println("error occured")
-// 	// }
-// 	http.HandleFunc("/", handler)
-// 	http.HandleFunc("/hi", handler2)
-// 	var err error= http.ListenAndServe(":9090",nil)
-
-// 	if err != nil{
-// 		log.Fatal(err)
-// 	}
-
-// }
-func main() {
-
-	http.HandleFunc("/payment", handlePaymentIntent)
-	http.HandleFunc("/register", handleRegister)
-	var err error = http.ListenAndServe(":8083", nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
 func handlePaymentIntent(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		fmt.Println(http.StatusMethodNotAllowed)
+		log.Println("Method not allowed")
+		return
 	}
-	return
-}
-func handleRegister(w http.ResponseWriter, r *http.Request) {
 
-	response := []byte("server is running")
+	var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"First_name"`
+		LastName  string `json:"Last_name"`
+		Address1  string `json:"address_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		log.Println("Error decoding request:", err)
+		return
+	}
+
+	log.Printf("Processing payment for %s %s, Product: %s", req.FirstName, req.LastName, req.ProductId)
+
+	param := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calculateAmount(req.ProductId)),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+	paymentIntent, err := paymentintent.New(param)
+	if err != nil {
+		http.Error(w, "Failed to create payment intent", http.StatusInternalServerError)
+		log.Println("Error creating payment intent:", err)
+		return
+	}
+
+	// Send response to the client
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(paymentIntent)
+}
+
+func calculateAmount(product string) int64 {
+	switch product {
+	case "phone":
+		return 1000
+	case "calc":
+		return 2000
+	case "laptop":
+		return 3000
+	default:
+		return 0
+	}
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	response := []byte(`{"message":"server is running"}`)
+	w.Header().Set("Content-Type", "application/json")
 	_, err := w.Write(response)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error writing response:", err)
 	}
-	fmt.Println("register")
+	log.Println("Register endpoint hit")
+}
+
+func main() {
+	http.HandleFunc("/payment", handlePaymentIntent)
+	http.HandleFunc("/register", handleRegister)
+
+	log.Println("Server is running on port 8083")
+	err := http.ListenAndServe(":8083", nil)
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
